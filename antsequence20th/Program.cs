8f7973f6190c0eee371202a5924b8b3f1a5ffa6e
KimYeeun <s2018s01@e-mirim.hs.kr>
2020-05-12 20:22:55 +0900

    개미수열

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,35 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            string ans = "1";
+            int i = 1;
+            while (i <= 20)
+            {
+                Console.WriteLine(i + "번째 수열 : " + ans);
+                int count = 0;
+                char token = ans[0];
+                string ans2 = "";
+                for (int j=0; j<ans.Length; j++)
+                {
+                    if (token == ans[j])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        ans2 += "" + token + count;
+                        count = 0;
+                        token = ans[j];
+                        count++;
+                    }
+                    if (j + 1 == ans.Length)
+                    {
+                        ans2 += "" + token + count;
+                    }
+                }
+                ans = ans2;
+                i++;
+            }
         }
     }
 }
